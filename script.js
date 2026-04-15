3c09e3de6962b1c2acf8276f38119f2038f5fcb4
MarlonSantanaCSUN <marlon.santana.621@my.csun.edu>
2026-04-15 12:27:23 -0700

    Update script

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-$(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
-    
-    // Called function to update the name, happiness, and weight of our pet in our HTML
-    checkAndUpdatePetInfoInHtml();
+$(function() {
+  // Initialize display
+  checkAndUpdatePetInfoInHtml();
   
-    // When each button is clicked, it will "call" function for that button (functions are below)
-    $('.treat-button').click(clickedTreatButton);
-    $('.play-button').click(clickedPlayButton);
-    $('.exercise-button').click(clickedExerciseButton);
+  // Button click events
+  $('.treat-button').click(clickedTreatButton);
+  $('.play-button').click(clickedPlayButton);
+  $('.exercise-button').click(clickedExerciseButton);
+  $('.sleep-button').click(clickedSleepButton); // NEW BUTTON
   
-
+  });
   
-    
-  })
-  
-    // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
+    // Pet object
+    var pet_info = {
+      name: "Buddy",
+      weight: 50,
+      happiness: 50
+    };
   
     function clickedTreatButton() {
       // Increase pet happiness
